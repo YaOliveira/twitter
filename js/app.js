ddb7cbd545eec568e810d77926ed57d72414e3e7
YaOliveira <magicsunshine_ya@msn.com>
2019-02-19 13:38:28 -0300

    Melhorias no layout e css

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//pedir o nome para começar a jogar
+let txt = document.getElementById("msg").value;
+
+//publicar o nome na tela
+let reply = document.getElementsByClassName("messageHere");
+reply.innerHTML = txt;;
