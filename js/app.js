12f890c0343af87f93bb2607b3850be5de39a971
YaOliveira <magicsunshine_ya@msn.com>
2019-02-20 14:41:58 -0300

    Início da versão 2 do twitter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-//pedir o nome para começar a jogar
-let txt = document.getElementById("msg").value;
+let btnPrint = document.querySelector('.btnTweetar');
 
-//publicar o nome na tela
-let reply = document.getElementsByClassName("messageHere");
-reply.innerHTML = txt;;
+function printMensagem(){
+    let tweete = document.getElementById("msg").value; 
+    document.querySelector('#messageHere').innerHTML = tweete;
+}
+
+btnPrint.addEventListener('click', function(){
+  printMensagem();
+  event.preventDefault();
+});
+
+// // versão 2
+
+window.doSomething = function(input) {
+    document.getElementById('btntweetar').disabled = (input.value.length == 0);
+}
+
+/*função para contador regressivo de caracteres da textarea*/
+
+
+$(document).on("keydown", "#TxtObservacoes", function () {
+    var caracteresRestantes = 255;
+    var caracteresDigitados = parseInt($(this).val().length);
+    var caracteresRestantes = caracteresRestantes - caracteresDigitados;
+    
+    $(".caracteres").text(caracteresRestantes);
+});
