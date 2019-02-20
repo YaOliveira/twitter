926a93695a0604d867699a56cc8169d56052dda9
YaOliveira <magicsunshine_ya@msn.com>
2019-02-20 17:32:32 -0300

    Inserção de cor e contador

--- js/app.js
+++ js/app.js
@@ -1,28 +1,59 @@
+/* printar a mensagem postada pelo usuário */
 let btnPrint = document.querySelector('.btnTweetar');
 
-function printMensagem(){
-    let tweete = document.getElementById("msg").value; 
+function printMensagem() {
+    let tweete = document.getElementById("msg").value;
     document.querySelector('#messageHere').innerHTML = tweete;
 }
 
-btnPrint.addEventListener('click', function(){
-  printMensagem();
-  event.preventDefault();
+btnPrint.addEventListener('click', function () {
+    printMensagem();
+    event.preventDefault();
 });
+
+
 
 // // versão 2
 
-window.doSomething = function(input) {
-    document.getElementById('btntweetar').disabled = (input.value.length == 0);
+window.doSomething = function (input) {
+    document.getElementById('btntweetar').disabled = (input.value.length === 0);
 }
 
 /*função para contador regressivo de caracteres da textarea*/
+/* contador de caracteres */
 
-
-$(document).on("keydown", "#TxtObservacoes", function () {
-    var caracteresRestantes = 255;
-    var caracteresDigitados = parseInt($(this).val().length);
-    var caracteresRestantes = caracteresRestantes - caracteresDigitados;
+function caracteres() {
+    let max = 10 ;
+    let atual = document.getElementById("msg").value.length;  
+    let valor = max - atual;
+    console.log(valor);
     
-    $(".caracteres").text(caracteresRestantes);
-});
+    
+    if (valor > 0) {
+        document.getElementById("contadorHere").innerHTML = valor + " caracteres restantes";
+        document.getElementsByClassName('btnTweetar')[0].disabled = false;
+        
+     }else if (valor < 0) {
+        document.getElementById("contadorHere").innerHTML = valor;
+        document.getElementsByClassName('btnTweetar')[0].disabled = true;
+        document.getElementById("contadorHere").style.color='red';
+    }
+}
+
+
+
+
+// function contaCaracteres(){
+//     let caracteresRestantes = 255;
+//     let caracteresDigitados = document.getElementById("msg").value.length;
+//     let caracteresRestantes = caracteresRestantes - caracteresDigitados;
+
+//     return contaCaracteres;
+// }
+
+// function printContador(contaCaracteres){
+//     let contador = contaCaracteres; 
+//     document.querySelector('#contadorHere').innerHTML = contador;
+// }
+
+
